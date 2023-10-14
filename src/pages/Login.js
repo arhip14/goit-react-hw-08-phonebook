01f6869adaf4c895f73fc323e911d11290d0ca06
arhip14 <kruvdyk1503@gmail.com>
2023-10-14 20:11:52 +0300

    update files

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,10 @@
-import { Helmet } from 'react-helmet';
 import { LoginForm } from 'components/LoginForm/LoginForm';
 
 export default function Login() {
   return (
     <div>
-      <Helmet>
-        <title>Login</title>
-      </Helmet>
+      <title>Login</title>
+
       <LoginForm />
     </div>
   );
