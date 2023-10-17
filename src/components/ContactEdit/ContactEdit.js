7d7d4dda57824872544f1ab78547541e0f15e015
arhip14 <kruvdyk1503@gmail.com>
2023-10-17 22:04:12 +0300

    add formik

--- src/components/ContactEdit/ContactEdit.js
+++ src/components/ContactEdit/ContactEdit.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addNewContact } from 'redux/contacts/thunk';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const formStyle = {
   maxWidth: '300px',
@@ -30,48 +32,57 @@ const buttonStyle = {
   fontSize: '16px',
 };
 
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .matches(/^[A-Za-z\s]+$/, 'Name is not valid')
+    .required('Name is required'),
+  number: Yup.string()
+    .matches(/^\d+$/, 'Number is not valid')
+    .required('Number is required'),
+});
+
 export const ContactEditor = () => {
   const dispatch = useDispatch();
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (values) => {
+    const { name, number } = values;
 
-    if (name.trim() !== '' && number.trim() !== '') {
+    if (name && number) {
       try {
         await dispatch(addNewContact({ name, number }));
-        setName('');
-        setNumber('');
       } catch (error) {
         alert('An error occurred while adding the contact.');
       }
     } else {
-      alert('Enter Name and Number !!!');
+      alert('Enter a valid Name and Number.');
     }
   };
 
   return (
-    <form style={formStyle} onSubmit={handleSubmit}>
-      <input
-        style={inputStyle}
-        type="text"
-        name="name"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        style={inputStyle}
-        type="text"
-        name="number"
-        placeholder="Number"
-        value={number}
-        onChange={(e) => setNumber(e.target.value)}
-      />
-      <button style={buttonStyle} type="submit">
-        Add Contact
-      </button>
-    </form>
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form style={formStyle}>
+        <Field
+          style={inputStyle}
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+        <ErrorMessage name="name" component="div" style={{ color: 'red' }} />
+        <Field
+          style={inputStyle}
+          type="text"
+          name="number"
+          placeholder="Number"
+        />
+        <ErrorMessage name="number" component="div" style={{ color: 'red' }} />
+        <button style={buttonStyle} type="submit">
+          Add Contact
+        </button>
+      </Form>
+    </Formik>
   );
 };
